e646a9785fbd9afa2a26e33cb08a34e2f0d58493
andr3w321 <andr3w321@gmail.com>
2023-07-05 11:19:07 -0700

    updated url

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 today=$(date +"%m-%d-%Y")
-curl https://spotgamma-system-files.s3.amazonaws.com/SPXstj22.csv > ./data/ES-${today}.csv
+curl https://spotgamma-system-files.s3.amazonaws.com/SPXstyoy.csv > ./data/ES-${today}.csv
+
